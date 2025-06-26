89656758d28f3a33623c31a88ae3e36b4e58f821
themystic1 <cluckyugo@gmail.com>
2025-06-26 17:54:47 +0100

    jfbc

--- app/jobs/layout.tsx
+++ app/jobs/layout.tsx
@@ -1,5 +1,11 @@
 import JobsClientLayout from "@/components/jobs/jobs-client-layout";
+import { Metadata } from "next";
 import { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "List job",
+  description: "List your job and get bids from professionals on the field",
+};
 
 const JobsLayout = ({ children }: { children: ReactNode }) => {
   return <JobsClientLayout>{children}</JobsClientLayout>;
